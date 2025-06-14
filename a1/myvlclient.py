20dc81ae4cfa45bd130407a9d97d849d1b3e83d9
Anranhhh <148399911+Anranhhh@users.noreply.github.com>
2025-06-14 14:10:10 -0700

    Update myvlclient.py

--- a1/myvlclient.py
+++ a1/myvlclient.py
@@ -1,1 +1,24 @@
+from socket import *
 
+serverName = 'localhost' # ip address
+serverPort = 12000 # port number
+
+# TCP SOCKET_STREAM
+clientSocket = socket(AF_INET, SOCK_STREAM)
+# Connect to the server
+clientSocket.connect((serverName, serverPort))
+
+# Ask user for input
+user_input = input('Input lowercase sentence: ')
+
+# Send the input message to the server
+clientSocket.send(user_input.encode())
+
+# Receive the modified sentence from the server
+modifiedSentence = clientSocket.recv(64) 
+
+# Print the modified sentence
+print('From Server: ', modifiedSentence.decode()) 
+
+# Close the connection
+clientSocket.close()
